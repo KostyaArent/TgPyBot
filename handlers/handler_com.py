93fdb95d9d7b19a831bd508efa443b22ddfe4a3c
kos <katrepalin@gmail.com>
2022-11-29 12:34:08 +0300

    bot start interface added

---
+++ handlers/handler_com.py
@@ -0,0 +1,26 @@
+from handlers.handler import Handler
+
+
+class HandlerCommand(Handler):
+    """
+    Класс обрабатывает входящие команды /start и /help и т.п.
+    """
+
+    def __init__(self, bot):
+        super().__init__(bot)
+
+    def pressed_btn_start(self, message):
+        """
+        обрабатывает входящие /start команды
+        """
+        self.bot.send_message(message.chat.id,
+                              f"{message.from_user.first_name},"
+                              f" здравствуйте! Жду дальнейших задач.",
+                              reply_markup=self.keybords.start_menu())
+
+    def handle(self):
+        @self.bot.message_handler(command=['start'])
+        def handle(message):
+            print(type(message))
+            if message.text == '/start':
+                self.pressed_btn_start(message)
--- handlers/handler_main.py
+++ handlers/handler_main.py
@@ -1,9 +1,13 @@
+from handlers.handler_com import HandlerCommand
+
+
 class HandlerMain:
     """
     Класс компоновщик
     """
     def __init__(self, bot):
         self.bot = bot
+        self.handler_commands = HandlerCommand(self.bot)
 
     def handle(self):
-        pass
+        self.handler_commands.handle()
